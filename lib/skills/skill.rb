c14f81ca6bdb52ddacdc36c86db9e8c3cd5a39db
Markus Unger <schaulustig23@gmail.com>
2018-12-31 11:35:56 +0100

    fixed circular argument reference warning on startup

--- lib/skills/skill.rb
+++ lib/skills/skill.rb
@@ -5,7 +5,13 @@
 class Skill
   attr_reader :name, :cost, :range, :target, :description
 
-  def initialize(name: name, cost: cost, range: range, target: target, description: description)
+  def initialize(
+      name: 'generic skill',
+      cost: 0,
+      range: 0,
+      target: :self,
+      description: 'does absolutely nothing'
+    )
     @name   = name
     @cost   = cost
     @range  = range
